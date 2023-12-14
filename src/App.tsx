feb82d8056155d63f51f36a324a6930e29dd622d
silvak <silvak.jeg@gmail.com>
2023-12-14 07:43:58 -0400

    project init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <main>
+      <div>
+        <h1>Javascript Quizz</h1>
+      </div>
+    </main>
+  );
+}
+
+export default App;
