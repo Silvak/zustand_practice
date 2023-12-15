eb1556866b266186d32ad4cd7481727f61c35fbb
silvak <silvak.jeg@gmail.com>
2023-12-15 19:28:34 -0400

    add use dev tools

--- src/store/questions.ts
+++ src/store/questions.ts
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import { type Question } from "../types";
 import confetti from "canvas-confetti";
-import { persist } from "zustand/middleware";
+import { persist, devtools } from "zustand/middleware";
 
 interface State {
   questions: Question[];
@@ -13,73 +13,84 @@ interface State {
 }
 
 export const useQuestionStore = create<State>()(
-  persist(
-    (set, get) => {
-      return {
-        loading: false,
-        questions: [],
-        currentQuestion: 0,
+  devtools(
+    persist(
+      (set, get) => {
+        return {
+          loading: false,
+          questions: [],
+          currentQuestion: 0,
 
-        fetchQuestion: async (limit: number) => {
-          const res = await fetch("http://localhost:5173/data.json");
-          const json = await res.json();
+          fetchQuestion: async (limit: number) => {
+            const res = await fetch("http://localhost:5173/data.json");
+            const json = await res.json();
 
-          const questions = json
-            .sort(() => Math.random() - 0.5)
-            .slice(0, limit);
-          set({ questions });
-        },
+            const questions = json
+              .sort(() => Math.random() - 0.5)
+              .slice(0, limit);
+            //set({ questions });
+            set({ questions }, false, "FETCH_QUESTIONS"); //devtools
+          },
 
-        selectAnswer: (questionId: number, answeIndex: number) => {
-          const { questions } = get();
+          selectAnswer: (questionId: number, answeIndex: number) => {
+            const { questions } = get();
 
-          // structuredClone -> clonacion de objeto de fornma profunda
-          const newQuestion = structuredClone(questions);
-          // encontramos el indice de la pregunta
-          const questionIndex = newQuestion.findIndex(
-            (q) => q.id == questionId
-          );
-          const questionInfo = newQuestion[questionIndex];
-          // averiguamos si usuario seleciono la respuesta correcta
-          const isCorrectUserAnswer = questionInfo.correctAnswer == answeIndex;
+            // structuredClone -> clonacion de objeto de fornma profunda
+            const newQuestion = structuredClone(questions);
+            // encontramos el indice de la pregunta
+            const questionIndex = newQuestion.findIndex(
+              (q) => q.id == questionId
+            );
+            const questionInfo = newQuestion[questionIndex];
+            // averiguamos si usuario seleciono la respuesta correcta
+            const isCorrectUserAnswer =
+              questionInfo.correctAnswer == answeIndex;
 
-          if (isCorrectUserAnswer) confetti();
+            if (isCorrectUserAnswer) confetti();
 
-          //cambiar informaicon en la copai de la pregunta
-          newQuestion[questionIndex] = {
-            ...questionInfo,
-            isCorrectUserAnswer,
-            userSelectedAnswer: answeIndex,
-          };
+            //cambiar informaicon en la copai de la pregunta
+            newQuestion[questionIndex] = {
+              ...questionInfo,
+              isCorrectUserAnswer,
+              userSelectedAnswer: answeIndex,
+            };
 
-          //actualizar estado
-          set({ questions: newQuestion });
-        },
+            //actualizar estado
+            //set({ questions: newQuestion });
+            set({ questions: newQuestion }, false, "SELECT_ANSWER");
+          },
 
-        goNextQuestion: () => {
-          const { currentQuestion, questions } = get();
-          const nextQuestion = currentQuestion + 1;
+          goNextQuestion: () => {
+            const { currentQuestion, questions } = get();
+            const nextQuestion = currentQuestion + 1;
 
-          if (nextQuestion < questions.length) {
-            set({ currentQuestion: nextQuestion });
-          }
-        },
+            if (nextQuestion < questions.length) {
+              //set({ currentQuestion: nextQuestion });
+              set({ currentQuestion: nextQuestion }, false, "GO_NEXT_QUESTION");
+            }
+          },
 
-        goPreviousQuestion: () => {
-          const { currentQuestion } = get();
-          const previoudQuestion = currentQuestion - 1;
+          goPreviousQuestion: () => {
+            const { currentQuestion } = get();
+            const previoudQuestion = currentQuestion - 1;
 
-          if (previoudQuestion > 0) {
-            set({ currentQuestion: previoudQuestion });
-          }
-        },
-        reset: () => {
-          set({ currentQuestion: 0, questions: [] });
-        },
-      };
-    },
-    {
-      name: "questions",
-    }
+            if (previoudQuestion > 0) {
+              set({ currentQuestion: previoudQuestion });
+            }
+          },
+          reset: () => {
+            //set({ currentQuestion: 0, questions: [] });
+            set(
+              { currentQuestion: 0, questions: [] },
+              false,
+              "GO_PREVIOUS_QUESTION"
+            );
+          },
+        };
+      },
+      {
+        name: "questions",
+      }
+    )
   )
 );
